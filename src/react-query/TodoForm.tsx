7d3247ebe24cc47f6aa60361b96be8ad98438577
damini <daminisaxena2@gmail.com>
2023-07-15 10:31:53 +0530

    Enhancing user experience

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -9,7 +9,7 @@ import { Todo } from "./hooks/useTodo";
 
 const TodoForm = () => {
   const queryClient = useQueryClient();
-  const addTodo = useMutation({
+  const addTodo = useMutation<Todo, Error, Todo>({
     mutationFn: (todo: Todo) => {
       return axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
@@ -20,31 +20,39 @@ const TodoForm = () => {
         addedTodo,
         ...(todos || []),
       ]);
+      if (ref.current) ref.current.value = "";
     },
   });
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form
-      className="row mb-3"
-      onSubmit={(event) => {
-        event.preventDefault();
-        if (ref.current && ref.current.value)
-          addTodo.mutate({
-            id: 0,
-            title: ref.current?.value,
-            userId: 1,
-            completed: true,
-          });
-      }}
-    >
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
+    <>
+      {addTodo.error && (
+        <div className="alert alert-danger">{addTodo.error.message}</div>
+      )}
+      <form
+        className="row mb-3"
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (ref.current && ref.current.value)
+            addTodo.mutate({
+              id: 0,
+              title: ref.current?.value,
+              userId: 1,
+              completed: true,
+            });
+        }}
+      >
+        <div className="col">
+          <input ref={ref} type="text" className="form-control" />
+        </div>
+        <div className="col">
+          <button className="btn btn-primary" disabled={addTodo.isLoading}>
+            {addTodo.isLoading ? "Adding" : "Add"}
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
